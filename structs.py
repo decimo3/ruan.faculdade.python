1cf5c7ef9b11381fc8a31343823249c75d709875
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-02-25 19:03:51 -0300

    Structs in python

---
+++ structs.py
@@ -0,0 +1,16 @@
+# ESTRUTURAS DE DADOS EM PYTHON
+# Em Python, existem objetos que são usados para armazenar mais de um valor, também chamados de estruturas de dados.
+# Cada objeto é capaz de armazenar um tipo de estrutura de dados, particularidade essa que habilita funções diferentes para cada tipo.
+# Portanto, a escolha da estrutura depende do problema a ser resolvido.
+# Para nosso estudo, vamos dividir as estruturas de dados em objetos do tipo sequência, do tipo set, do tipo mapping e do tipo array NumPy.
+# Cada grupo pode possuir mais de um objeto. Vamos estudar esses objetos na seguinte ordem:
+
+#1. Objetos do tipo sequência: texto, listas e tuplas.
+#2. Objetos do tipo set (conjunto).
+#3. Objetos do tipo mapping (dicionário).
+#4. Objetos do tipo array NumPy.
+
+# Os objetos do tipo sequência são estruturas de dados capazes de armazenar mais de um valor.
+# Essas estruturas de dados representam sequências finitas indexadas por números não negativos.
+
+
